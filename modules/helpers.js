52dde942e17eaac3496a4413bad4fd7ef47c58c2
System <none>
2017-08-19 17:54:52 +0000

    Refactored and verified all errors are caught now

--- modules/helpers.js
+++ modules/helpers.js
@@ -1,14 +1,15 @@
 'use strict'
 
-// Validates a url and simply returns that url if it is valid, else returns undefined
 const validUrl = require('valid-url')
 
+// Validates a url and simply returns that url if it is valid, else returns undefined
 const validate = (url) => {
   return new Promise((resolve, reject) => {
     resolve(validUrl.isWebUri(url))
   })
 }
 
+// Still need to implement this as the error catcher for all routes
 function asyncWrap(fn) {  
   return (req, res, next) => {
     fn(req, res, next).catch(next);
